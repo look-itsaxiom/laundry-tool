7c0b0d775237159ecc700f8357b6e5e335e0aa35
Chase Skibeness <chase.skibeness@tapcheck.com>
2025-10-31 16:10:00 -0700

    compatibility issues

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,14 +1,22 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import legacy from "@vitejs/plugin-legacy";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    legacy({
+      targets: ["defaults", "not IE 11", "Android >= 6", "ChromeAndroid >=80"],
+      additionalLegacyPolyfills: ["regenerator-runtime/runtime"],
+    }),
+  ],
   server: {
     port: 3000,
     open: true,
   },
   build: {
+    target: "es2015",
     outDir: "build",
   },
   test: {
